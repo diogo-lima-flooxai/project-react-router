0a6bbcdd038ff4c45b5db45ebe731c803562d129
diogo <diogo.lima.flooxai@gmail.com>
2025-04-14 10:20:44 -0300

    feat: starting project and structuring

---
+++ navegacao/src/components/layout/Content.jsx
@@ -0,0 +1,9 @@
+import './Content.css'
+
+const Content = props => (
+  <main className="Content">
+    Component Content
+  </main>
+)
+
+export default Content
---
+++ navegacao/src/views/examples/Home.jsx
@@ -0,0 +1,9 @@
+
+const Home = props => (
+  <div className="Home">
+    <h1>Inicio</h1>
+    <p>Bem Vindo!</p>
+  </div>
+)
+
+export default Home
