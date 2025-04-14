d64e3d96099b56924296ee7972cbdc386779ac46
diogo <diogo.lima.flooxai@gmail.com>
2025-04-14 11:01:20 -0300

    feat: navigating over simple pages

--- navegacao/src/components/layout/Content.jsx
+++ navegacao/src/components/layout/Content.jsx
@@ -1,9 +1,18 @@
-import './Content.css'
+import "./Content.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom"; 
+ 
+import Home from "../../views/examples/Home";
+import About from "../../views/examples/About";
 
+ 
 const Content = props => (
   <main className="Content">
-    Componente Content
+    <Routes>
+      <Route path="/about" element={<About />} />
+      <Route path="/" exact element={<Home />} />
+    </Routes>
   </main>
-)
-
-export default Content
+);
+ 
+export default Content;
--- navegacao/src/views/App.jsx
+++ navegacao/src/views/App.jsx
@@ -1,12 +1,16 @@
-import Content from '../components/layout/Content'
-import Menu from '../components/layout/Menu'
-import './App.css'
+import { BrowserRouter } from "react-router-dom";
 
-const App = props => (
+import Content from "../components/layout/Content";
+import Menu from "../components/layout/Menu";
+import "./App.css";
+
+const App = (props) => (
   <div className="App">
-    <Menu />
-    <Content/>
+    <BrowserRouter>
+      <Menu />
+      <Content />
+    </BrowserRouter>
   </div>
-)
+);
 
-export default App
+export default App;
